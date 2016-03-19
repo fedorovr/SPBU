d123f89c86489a791573850ab1ad0164992e87e6
Artem Lobanov <resaglow@gmail.com>
2016-03-19 17:44:28 +0300

    Add dict, coords & split times

--- course_work_2016/cw.py
+++ course_work_2016/cw.py
@@ -1,9 +1,16 @@
-import pandas
 from datetime import datetime, timedelta
 
-xl = pandas.ExcelFile("m1.xlsx")
+import numpy as np
+import pandas as pd
 
-df = xl.parse("Sheet1")
+from sklearn.cluster import MiniBatchKMeans
+
+# xl = pd.ExcelFile("m1.xlsx")
+# df = xl.parse('Sheet1')
+
+df = pd.read_csv('old_petrovsky_11.csv')
+
+report_df = pd.read_csv('report.csv')
 
 
 def index_of_min(values):
@@ -14,37 +21,69 @@ def get_time(x):
     return datetime.strptime(x, "%d.%m.%Y %H.%M.%S")
 
 
-from_time = get_time("20.10.2015 19.00.00")
-to_time = get_time("20.10.2015 21.00.00")
+from_time = get_time("20.10.2015 11.00.00")
+to_time = get_time("20.10.2015 15.00.00")
 
 
-def create_aligned_path(data):
-	lacs, times = data["LAC"], data["Call time"]
-	all_time = to_time - from_time
-	step = timedelta(minutes=30)
-	current_time = from_time
-	acc = []
-	while current_time <= to_time:
-		time_differences = [abs(current_time - get_time(some_time)) for some_time in times]
-		index = index_of_min(time_differences)
-		acc.append(lacs[index])
-		current_time += step
-	return acc
+def create_aligned_path(data, start_time, end_time, interval):
+    lacs, times = data["LAC"], data["Время вызова"]
+    step = timedelta(minutes=interval)
+    current_time = start_time
+    acc = []
+    while current_time <= end_time:
+        time_differences = [abs(current_time - get_time(some_time)) for some_time in times]
+        index = index_of_min(time_differences)
+        acc.append(lacs[index])
+        current_time += step
+    return acc
+
 
 def between_time(x):
-	return to_time > get_time(x) > from_time
+    return to_time > get_time(x) > from_time
 
-# Firstly, chooses only events in some time period
-# Secondly, groups people by code - unique number for every user
-# After all collects all LAC's (base stations) for every user in a list
-lacs = df[df["Call time"].apply(between_time)].groupby("Code")["LAC"].apply(lambda x: x.tolist())
+# Choose only events in some time period
+# Group people by code - unique number for every user
+# Collect all LACs (base stations) for every user in a list
+
+df1 = df[df['Время вызова'].apply(between_time)]
+df2 = df1.dropna(subset=['LAC'])
+
+lacs = df2.groupby('Код')['LAC'].apply(lambda x: x.tolist())
 # The same with all times for every LAC
-times = df[df["Call time"].apply(between_time)].groupby("Code")["Call time"].apply(lambda x: x.tolist())
+times = df2.groupby('Код')['Время вызова'].apply(lambda x: x.tolist())  # TODO Is double groupby bad?
 
 # Join 2 series to dataframe on Code
-data = pandas.concat([lacs, times], axis=1).reset_index()
+data = pd.concat([lacs, times], axis=1).reset_index()
 
 # Add alignded LACs sequences
-data['Path'] = data.apply(create_aligned_path, axis=1)
+data['Path'] = data.apply(lambda x: create_aligned_path(x, from_time, to_time, 30), axis=1)
 
-print(data)
+report_df.drop_duplicates(subset=['LAC'], inplace=True)
+
+report_dict = report_df.set_index('LAC').T.to_dict('list')
+
+
+def get_coords(path):
+    path_ret = []
+    for lac in path:
+        lac_info = report_dict[lac]
+        lon, lat = lac_info[1], lac_info[2]
+        path_ret += [lon, lat]
+
+    return path_ret
+
+
+def split_timespan(chunk_count):
+    delta = (to_time - from_time) / chunk_count
+    return [to_time + delta * idx for idx in range(chunk_count)]
+
+
+coord_list = data['Path'].apply(get_coords).tolist()
+
+rng = np.random.RandomState(0)
+kmeans = MiniBatchKMeans(n_clusters=2, random_state=rng, verbose=True)
+
+kmeans.partial_fit(coord_list)
+
+for i, patch in enumerate(kmeans.cluster_centers_):
+    print("Cluster #" + str(i) + ": " + str(patch))
